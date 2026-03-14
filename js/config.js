216e6916d1240c2690d990b7781e4ac32a06e906
razuwilca <4centauro@gmail.com>
2026-03-14 14:15:20 -0500

    cumpleaños

--- js/config.js
+++ js/config.js
@@ -21,31 +21,3 @@ tailwind.config = {
     },
   },
 };
-
-function drawX() {
-  for (let i = 0; i < 5; i++) {
-    let row = ""; // Creamos un texto vacío para esta fila
-    for (let j = 0; j < 5; j++) {
-      if (i === j || i + j === 4) {
-        row += "*"; // Concatenamos el asterisco
-      } else {
-        row += " "; // Concatenamos el espacio
-      }
-    }
-    console.log(row); // Imprimimos la fila completa antes de pasar a la siguiente
-  }
-}
-
-drawX();
-
-function drawx2() {
-  let array = [
-    ["*", " ", " ", " ", "*"],
-    [" ", "*", " ", "*", " "],
-    [" ", " ", "*", " ", " "],
-    [" ", "*", " ", "*", " "],
-    ["*", " ", " ", " ", "*"],
-  ];
-  console.log(array.map((row) => row.join("")).join("\n"));
-}
-drawx2();
